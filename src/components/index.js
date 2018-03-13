c983eee4813614343d173fb9657ac694c54c1f94
fahmifan <fahmiirfan909@gmail.com>
2018-03-13 16:33:37 +0700

    export NavbarSigned

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { Navbar } from './Navbar/Navbar';
+export { NavbarSigned } from './NavbarSigned/NavbarSigned';
 export { Input } from './Input/Input';
 export { Text } from './Text/Text';
 export { ButtonWide } from './ButtonWide/ButtonWide';
