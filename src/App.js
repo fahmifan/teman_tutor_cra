567b01330d55dc9e1db205d73a7fc429e75b0696
fahmifan <fahmiirfan909@gmail.com>
2018-03-13 16:34:53 +0700

    add Home comps. to Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { LandingPage, LogIn, SignUp } from './pages';
+import { 
+  LandingPage, 
+  LogIn, 
+  SignUp,
+  Home, 
+} from './pages';
 
 class App extends Component {
   render() {
@@ -10,6 +15,7 @@ class App extends Component {
         <Switch>
           <Route path="/login" component={LogIn} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/home" component={Home} />
           <Route path="/" component={LandingPage} />
         </Switch>
       </BrowserRouter>
