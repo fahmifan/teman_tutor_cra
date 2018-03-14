51212579d3fabc916868dbe2f5e9ad0215b7d4ae
fahmifan <fahmiirfan909@gmail.com>
2018-03-14 19:15:31 +0700

    Add Background comps.

---
+++ src/components/Background/Background.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  box-sizing: border-box;
+  position: relative;
+  width: 1024px;
+  height: 100%;
+  background-color: #fbfbfb;
+  margin: 0 auto;
+  padding-top: 10px;
+  z-index: -1;
+`
