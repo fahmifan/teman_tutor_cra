218b7b1af9849812c64c118c33af043d33f9c88c
fahmifan <fahmiirfan909@gmail.com>
2018-03-15 09:37:08 +0700

    update Text comps

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -5,9 +5,10 @@ import style from '../../assets/style';
 export const Text = styled.p`
   padding: 0;
   margin: 0;
-  padding: 5px;
-  font-size: ${props => props.fontSize}px;
-  color: ${props => props.color};
-  opacity: ${props => props.opacity};
+  padding: ${props => props.padding || 5}px;
+  font-size: ${props => props.fontSize || 12}px;
+  color: ${props => props.color || style.colors.black};
+  opacity: ${props => props.opacity || 1};
   font-weight: ${props => props.bold && 700};
+  ${props => props.timeline && 'align-self: flex-start;'}
 `
