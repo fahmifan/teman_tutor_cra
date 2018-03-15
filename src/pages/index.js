a2f51fd8e463343d1fc6153761919341b9c532c7
fahmifan <fahmiirfan909@gmail.com>
2018-03-16 06:40:12 +0700

    change Home export path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export {LandingPage} from './LandingPage';
 export {LogIn} from './LogIn';
 export {SignUp} from './SignUp';
-export {Home} from './Home';
+export {Home} from './Home/Home';
 export {Timeline} from './Timeline';
