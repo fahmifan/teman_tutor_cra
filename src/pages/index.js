537d38c55c8ae67a5e4af89a5b206237eb80bc97
dimassatriap <prakosodimad@gmail.com>
2018-03-16 18:24:35 +0700

    fixing explore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export {LandingPage} from './LandingPage';
 export {LogIn} from './LogIn';
 export {SignUp} from './SignUp';
-export {Home} from './Home';
+export {Home} from './Home/Home';
 export {Timeline} from './Timeline';
 export {Explore} from './Explore';
