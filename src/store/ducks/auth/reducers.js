75947c9f6720babcf7a6a0b2ffceb17ad6e21858
fahmifan <fahmiirfan909@gmail.com>
2018-03-18 10:46:02 +0700

    change credentials to user: {id, token, email}

--- src/store/ducks/auth/reducers.js
+++ src/store/ducks/auth/reducers.js
@@ -3,7 +3,11 @@ import  types from './types'
 const initState = {
     error: null,
     loading: false,
-    credentials: null,
+    user: {
+        id: 4,
+        token: 'h4GfDlYvSlNigJBUb76JZLx7ZbD06LtAco01BhKVXNaN5KN5MQMdN1dfwzuk',
+        email: null,
+    },
 }
 
 const reducer = (state = initState, action) => {
@@ -16,12 +20,23 @@ const reducer = (state = initState, action) => {
             ...state,
             loading: false,
             error: null,
-            credentials: action.credentials,
+            user: {
+                ...state.user,
+                token: action.token,
+                id: action,
+                email: action.email,    
+            }
         }
         case types.LOGIN_FAILED: return {
             ...state,
             loading: false,
             error: action.error,
+            user: {
+                ...state.user,
+                token: null,
+                id: null,
+                email: null,
+            }
         }
         default: return state
     }
