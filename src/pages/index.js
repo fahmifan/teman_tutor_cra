eab515c1c429f64c548ce74fbb82b895e3df99ee
fahmifan <fahmiirfan909@gmail.com>
2018-03-18 10:44:26 +0700

    update exported module

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ export {default as LogIn} from './LogIn';
 export {SignUp} from './SignUp';
 export {Home} from './Home/Home';
 export {Timeline} from './Timeline';
-export {Explore} from './Explore';
+export {default as Explore} from './Explore';
 export {default as BuatGrup} from './BuatGroup';
