fc45fdb7f194fa9e5efea12f347c7f5b5652f58f
fahmifan <fahmiirfan909@gmail.com>
2018-03-19 16:36:33 +0700

    fix login() & add logout()

--- src/store/ducks/auth/operations.js
+++ src/store/ducks/auth/operations.js
@@ -1,29 +1,42 @@
-import axios from '../../../axios';
+import axios from '../../../axios'
 import types from './types'
 import actions from './actions'
 
 const login = ({email, password}) => {
   return dispatch => {
       dispatch(actions.loginStart())
-      axios.post('/auth', {
-          email: email,
-          password: password
+      axios({
+        url:  '/auth', 
+        method: 'POST',
+        data: {
+          "email": email,
+          "password": password          
+        }
       })
       .then(response => {
-          const creds = response.data;
+          const {status, user} = response.data;
           const experationDate = new Date(new Date().getTime() + 3600 * 1000);
 
           localStorage.setItem('expirationDate', experationDate);
-          localStorage.setItem('token', creds.token);
-          localStorage.setItem('userId', creds.id);
-          localStorage.setItem('email', creds.email);
+          localStorage.setItem('token', user.remember_token);
+          localStorage.setItem('id', user.id);
+          localStorage.setItem('email', user.email);
 
-          dispatch(actions.loginSuccess(creds))
+          dispatch(actions.loginSuccess({status, user}))
       })
       .catch(error => {
-          dispatch(actions.loginFailed(error))
+        dispatch(actions.loginFailed(error))
       })
   }
+}
+
+const logout = () => {
+  const nowDate = new Date(new Date().getTime() + 3600 * 1000);
+  const experationDate = localStorage.getItem('expirationDate');
+
+  localStorage.removeItem('token');
+  localStorage.removeItem('email');
+  localStorage.removeItem('id');
 }
 
 export default { 
