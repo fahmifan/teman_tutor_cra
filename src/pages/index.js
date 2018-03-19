3e88d757ca7a340dc259b3812370d3cc3b424422
fahmifan <fahmiirfan909@gmail.com>
2018-03-20 01:16:15 +0700

    update exported pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export {LandingPage} from './LandingPage';
 export {default as LogIn} from './LogIn';
 export {SignUp} from './SignUp';
-export {Home} from './Home/Home';
+export {default as Home} from './Home/Home';
 export {Timeline} from './Timeline';
 export {default as Explore} from './Explore';
 export {default as BuatGrup} from './BuatGroup';
