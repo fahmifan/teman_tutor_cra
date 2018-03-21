292788195ec559f97d9f7b034ae046dc5ed27a36
fahmifan <fahmiirfan909@gmail.com>
2018-03-21 21:22:31 +0700

    change to export default as Timeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ export {LandingPage} from './LandingPage';
 export {default as LogIn} from './LogIn';
 export {SignUp} from './SignUp';
 export {default as Home} from './Home/Home';
-export {Timeline} from './Timeline';
+export {default as Timeline} from './Timeline';
 export {default as Explore} from './Explore';
 export {default as BuatGrup} from './BuatGroup';
