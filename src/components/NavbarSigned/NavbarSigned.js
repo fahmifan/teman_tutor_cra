bbff9d78a37c8a4fba1483249f67e5d65513c83b
fahmifan <fahmiirfan909@gmail.com>
2018-03-22 15:45:17 +0700

    change NavbarSigned into Statefull Component

--- src/components/NavbarSigned/NavbarSigned.js
+++ src/components/NavbarSigned/NavbarSigned.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { operations } from '../../store/ducks/auth';
 import styles from '../../assets/style';
-import { Text } from '../index';
+import { Text, ButtonWide, ButtonSmall } from '../index';
 import clockIcon from '../../assets/icons/clock-icon.svg';
 import teleskopIcon from '../../assets/icons/teleskop-icon.svg';
 import houseIcon from '../../assets/icons/house-icon.svg';
@@ -63,41 +65,97 @@ const Profile = styled.div`
   border-radius: 100%;
   text-align: center;
   margin-left: 8px;
+  &:hover {
+    cursor: pointer;
+  }
 `
 
-export const NavbarSigned = props => {
-  return (
-    <Nav>
-      <Link to="/explore">
-        <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleNight}>temanTutor</Text>
-      </Link>
-      <NavIconContainer>
-        <Link to="/home">
-          <NavIcon>
-            <img src={houseIcon} />
-            <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Home</Text>
-          </NavIcon>
-        </Link>
+const ProfileContainer = styled.div`
+  box-sizing: border-box;
+  width: 236px;
+  height: 71px;
+  background-color: #F2F2F2;
+  padding-left: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  position: relative;
+  border-bottom: 1px solid #eee;
+`
+
+const NotifContainer = styled.div`
+  margin-top: 68px;
+  box-sizing: border-box;
+  position: fixed;
+  right: 0;
+  z-index: 3;
+  margin-right: 30px;
+  box-shadow: 0px 2px 5px rgba(68,68,68,0.6);
+`
+
+export class NavbarSigned extends Component {
+
+  state = {
+    showProfile: false,
+  }
+
+  handleProfileClicked = () => {
+    this.setState({showProfile: !this.state.showProfile})
+  }
+
+  handleLogout = () => {
+    this.props.logout();
+  }
+
+  render() {
+    const { profileClicked } = this.props;
+    const { showProfile } = this.state;
+
+    return (
+      <Nav>
         <Link to="/explore">
-          <NavIcon>
-            <img src={teleskopIcon} />
-            <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Explore</Text>
-          </NavIcon>
+          <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleNight}>temanTutor</Text>
         </Link>
-        <Link to="/timeline">
-          <NavIcon>
-            <img src={clockIcon} />
-            <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Timeline</Text>
-          </NavIcon>
-        </Link>
-      </NavIconContainer>
-      <RightText>
-        <img src={bellIcon} />
-        <Link to="/buatgrup">
-          <Text bold color={styles.colors.skyBlue} fontSize={styles.fontSize[1]} >Buat Grup</Text>
-        </Link>
-        <Profile><span>J</span></Profile>
-      </RightText>
-    </Nav>
-  );
+        <NavIconContainer>
+          <Link to="/home">
+            <NavIcon>
+              <img src={houseIcon} />
+              <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Home</Text>
+            </NavIcon>
+          </Link>
+          <Link to="/explore">
+            <NavIcon>
+              <img src={teleskopIcon} />
+              <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Explore</Text>
+            </NavIcon>
+          </Link>
+          <Link to="/timeline">
+            <NavIcon>
+              <img src={clockIcon} />
+              <Text bold fontSize={styles.fontSize[2]} color={styles.colors.paleBlue}>Timeline</Text>
+            </NavIcon>
+          </Link>
+        </NavIconContainer>
+        <RightText>
+          <img src={bellIcon} />
+          <Link to="/buatgrup">
+            <Text bold color={styles.colors.skyBlue} fontSize={styles.fontSize[1]} >Buat Grup</Text>
+          </Link>
+          <Profile onClick={this.handleProfileClicked} ><span>J</span></Profile>
+          {
+            showProfile && <NotifContainer> 
+              <ProfileContainer>
+                <ButtonSmall onClick={this.handleLogout} fontSize={24} >Logout</ButtonSmall>
+              </ProfileContainer>
+            </NotifContainer>
+          }
+        </RightText>
+      </Nav>
+    );
+  }
 }
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(operations.logout()),
+})
+
+export default connect(null, mapDispatchToProps)(NavbarSigned);
